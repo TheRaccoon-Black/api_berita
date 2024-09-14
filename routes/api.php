4ebf598aeb35f45c339e421aa0a7f5e752e4fe9e
muhammad fachrurrozi <ozjibuwat@gmail.com>
2024-09-14 19:20:22 +0700

    get

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+// Route::get('/post', function () {
+//     return dd("berhasil post");
+// });
+
+Route::get('/posts',[PostController::class, 'index'])->name('posts.index');
